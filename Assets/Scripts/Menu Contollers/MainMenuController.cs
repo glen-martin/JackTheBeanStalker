b4698fb18b24faed35899d64ea36c4cae80facf1
Glen Martin <glen.martin@truecaller.com>
2020-02-29 15:36:25 +0530

    Add Scene transitions using SceneManager API. Transitions added btw main menu and gameplay, highscore and options menu and back button in all of them TODO: Music toggle button and Quit button behaviour:wq

---
+++ Assets/Scripts/Menu Contollers/MainMenuController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+
+    public void StartGame(){
+        SceneManager.LoadScene("Gameplay Scene");
+    }
+    public void OptionsMenu(){
+        SceneManager.LoadScene("Options Menu");
+    }
+    public void HighScoreMenu(){
+        SceneManager.LoadScene("High Score Menu");
+    }
+    public void MusicButton(){
+        
+    }
+    public void Quit(){
+        
+    }
+
+}
---
+++ Assets/Scripts/Menu Contollers/OptionsMenuController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OptionsMenuController : MonoBehaviour
+{
+    public void GoBack(){
+       SceneManager.LoadScene("Main Menu");
+   }
+}

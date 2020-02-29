23b5524fc5f615b78e84a2fdc0f7e5befabc91ac
Glen Martin <glen.martin@truecaller.com>
2020-02-29 12:59:46 +0530

    Set up player bounds based on scene bounds. Uses SpriteRenderer to get bounds of object, and camera to get bounds of camera and clamps player pos to screenWidth +- objectWidth/2

---
+++ Assets/Scripts/Player Scripts/PlayerBounds.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBounds : MonoBehaviour
+{
+    private float maxX , minX;
+
+    private float objectWidth;
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        objectWidth = GetComponent<SpriteRenderer>().bounds.size.x / 2;
+        SetMinAndMaxX();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 newPos = transform.position;
+        newPos.x = Mathf.Clamp(transform.position.x, minX + objectWidth, maxX- objectWidth);
+        transform.position = newPos;
+    }
+
+    void SetMinAndMaxX(){
+        Vector2 bounds = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        maxX = bounds.x;
+        minX = -bounds.x;
+    }
+}
